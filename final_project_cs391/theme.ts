858c246b0a0d5db2c0c978f4ee92da3ed4ad192d
leigh <ljbrown4@bu.edu>
2025-04-23 15:17:50 -0400

    added db and theme.ts

---
+++ final_project_cs391/theme.ts
@@ -0,0 +1,12 @@
+import { createTheme } from '@mui/material/styles'; //didn't like the blue focus thing
+
+
+const theme = createTheme({
+    palette: {
+        primary: {
+            main: '#5571b8',
+        },
+    },
+
+});
+export default theme;
