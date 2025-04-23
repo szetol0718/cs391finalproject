8a1e39845c1d96192d49116e3041fc77d95645f3
arooj <aroojkam7@gmail.com>
2025-04-23 13:41:16 -0400

    arooj

--- final_project_cs391/types.ts
+++ final_project_cs391/types.ts
@@ -1,8 +1,41 @@
-export type NoteType = {
-    note: string,
-    id: number,
+types.ts:
+
+//individual habits
+export type HType = //different types of habits user can track
+    "Health + Wellness" |
+    "Household" |
+    "Finance" |
+    "Personal" |
+    "Productivity";
+
+export type HTrack = //different ways user can track habits
+    "daily" |
+    "weekly" |
+    "bi-weekly";
+
+//needed for the checkbox completion
+export type Day = "sun" | "mon" | "tue" | "wed" | "thu" | "fri" | "sat";
+
+//each habit that is being tracked
+export type HabitProps = {
+    id: number;
+    habit: string;
+    type: HType;
+    tracking: HTrack;
+    notes?: string;
+    sun: boolean;
+    mon: boolean;
+    tue: boolean;
+    wed: boolean;
+    thu: boolean;
+    fri: boolean;
+    sat: boolean;
+    weeklyCompleted: number;
 }
 
-export type NumNotes = {
-    max?: number,
-}
+//stored weekly habit table to post previous weeks completion
+export type ArchiveProps = {
+    weekStart: string;
+    weekEnd: string;
+    habits: HabitProps[];
+};
