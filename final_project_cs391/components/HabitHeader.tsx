e202dbe1333cbe0b59aec5b34ba58449fbd02404
leigh <ljbrown4@bu.edu>
2025-04-28 15:03:45 -0400

    style

--- final_project_cs391/components/HabitHeader.tsx
+++ final_project_cs391/components/HabitHeader.tsx
@@ -5,7 +5,7 @@ import Link from "next/link";
 
 export default function HabitTrackerHeader() {
     const linkStyling =
-        "md:p-1 mb-2 md:m-2 text-lg md:text-xl font-bold hover:underline text-blue-950";
+        "md:p-1 mb-2 md:m-2 text-lg md:text-xl font-bold hover:underline text-green-950";
     const headerStyle =
         "w-full flex flex-row gap-4 justify-center items-center p-2 text-green-950 bg-teal-100";
 
