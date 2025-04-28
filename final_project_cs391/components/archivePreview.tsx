23d617b759351f4a7303b259f8f865251376003d
leigh <ljbrown4@bu.edu>
2025-04-28 19:22:33 -0400

    formatting

--- final_project_cs391/components/archivePreview.tsx
+++ final_project_cs391/components/archivePreview.tsx
@@ -5,12 +5,12 @@ import { ArchiveProps } from "@/types";
 export default function ArchivePreview({ archive }: { archive: ArchiveProps }) {
     return (
         <div className="rounded-lg p-4 m-3 w-[80%] mx-auto">
-            <h5 className="font-bold text-lg text-[#453750] mb-1">
+            <h5 className="font-bold text-lg text-green-900 mb-1">
                 Week: {new Date(archive.weekStart).toLocaleDateString()} -{" "}
                 {new Date(archive.weekEnd).toLocaleDateString()}
             </h5>
 
-            <div className="flex flex-wrap gap-6">
+            <div className="flex flex-wrap gap-6 justify-center">
                 {archive.habits.map((habit) => (
                     <div key={habit.id} className="flex flex-col sm:w-[50%] md:w-[40%] lg:w-[20%] p-2 border rounded-lg">
                         <div className="w-full flex justify-between mb-2">
