82daf375d777c0ab27e6707e91160ff510bcc939
leigh <ljbrown4@bu.edu>
2025-04-28 15:02:14 -0400

    style

--- final_project_cs391/app/archive/page.tsx
+++ final_project_cs391/app/archive/page.tsx
@@ -24,7 +24,7 @@ export default function ArchivePage() {
     }, []);
 
     return (
-        <main className="min-h-screen w-full flex-col justify-center pb-5 text-green-950 bg-green-300">
+        <main className="min-h-screen w-full flex-col justify-center pb-5 text-green-950 bg-teal-100">
             <HabitTrackerHeader/>
             <h1 className="text-4xl font-bold mb-4 p-6 text-center">Archive</h1>
             <ArchiveDisplay inputArchives={archive} />
