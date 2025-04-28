4255c15644a931b4d052dcc035140dd92c05822f
szetol0718 <szetol@bu.edu>
2025-04-27 20:36:40 -0400

    fix

--- final_project_cs391/db.ts
+++ final_project_cs391/db.ts
@@ -1,20 +1,29 @@
-// DELETE: Remove a task by ID
-export async function DELETE(req: NextRequest) {
-  const { id } = await req.json();
+import {MongoClient, Db, Collection} from 'mongodb';
+ 
+ const MONGO_URI = process.env.MONGO_URI as string;
+ if (!MONGO_URI) {
+     throw new Error("MONGO_URI environment variable is undefined")
+ }
+ const db_name = "final-project";
+ export const HABIT_COLLECTION = "habit-collection";  // habit collection
+ export const ARCHIVE_COLLECTION = "archive-collection";  // archive collection
+ export const NOTE_COLLECTION = "note-collection";
 
-  if (!id) {
-    return NextResponse.json({ error: 'Task ID is required' }, { status: 400 });
-  }
-
-  try {
-    await client.connect();
-    const db = client.db(dbName);
-    const collection = db.collection(collectionName);
-
-    const result = await collection.deleteOne({ _id: new ObjectId(id) });
-    return NextResponse.json(result);
-  } catch (error) {
-    console.error('DELETE error:', error);
-    return NextResponse.json({ error: 'Failed to delete task' }, { status: 500 });
-  }
-}
+ let client: MongoClient | null = null;
+ let db: Db | null = null;
+ 
+ async function connect(): Promise<Db> {
+     if (!client) {
+         client = new MongoClient(MONGO_URI);
+         await client.connect();
+     }
+     return client.db(db_name)
+ }
+ 
+ export default async function getCollection(
+     collectionName: string, ): Promise<Collection> {
+     if (!db) {
+         db= await connect();
+     }
+     return db.collection(collectionName);
+ }
